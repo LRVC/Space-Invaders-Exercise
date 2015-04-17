4bf175de89fec37054c2caee484770faface7490
LRVC <lrv378@gmail.com>
2015-04-16 21:17:35 -0600

    init commit

---
+++ game.js
@@ -0,0 +1,23 @@
+;(function() {
+  var Game = function (canvasId) {
+    var canvas = document.getElementById(canvasId);
+    var screen = canvas.getContext('2d');
+    var gameSize = { x: canvas.width, y: canvas.height };
+
+    var self = this;
+    var tick = function() {
+      self.update();
+      self.draw(screen, gameSize);
+    };
+
+    tick();
+  };
+
+  Game.prototype = {
+
+  };
+
+  window.onload = function() {
+    new Game("screen");
+  };
+})();
